cf8864da5fee49321920d552b0bdf51804246326
john <john@soda4.eden>
2023-01-21 18:46:44 -0800

    Bump Junit version Missing timezone in sunrise test

--- utility/src/test/java/test/net/sodacan/utility/TestSunrise.java
+++ utility/src/test/java/test/net/sodacan/utility/TestSunrise.java
@@ -13,6 +13,7 @@ public class TestSunrise {
 		        .on(2023, 1, 6)             // May 1st, 2020, starting midnight
 		        .latitude(42.557982)
 		        .longitude(-123.393342)
+		        .timezone("America/Los_Angeles")
 		        .execute();
 		System.out.println("Sunrise: " + ss.getRise());
 		System.out.println("Sunset:  " + ss.getSet());
